6fbc9cac5cf711838827592f9511cf2fce91cb59
Chittoor Vamsi <56587705+chittoorking@users.noreply.github.com>
2021-08-02 11:28:31 +0530

    Create temperature_monitor.js

---
+++ temperature_monitor.js
@@ -0,0 +1,5 @@
+setChartLibrary('google-chart');
+setChartTitle('Polynomial Regression');
+setChartType('predictionGraph');
+setAxisName('time_stamp','tempetarure in kelvin');
+plotChart('time_stamp','temp');
